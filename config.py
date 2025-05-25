a982fbf1cab306183556a9d18511adfdb338dd3a
Devin Reid <dreid97@gmail.com>
2025-05-25 18:56:32 -0500

    make file for saki san

--- config.py
+++ config.py
@@ -2,7 +2,15 @@
 FILES = {
     "search_log": "search_log.csv",
     "usage_counter": "usage_counter.txt",
-    "master_list": "master_list.csv",
-    "with_emails": "stores_with_emails.csv",
-    "without_emails": "stores_without_email.csv"
+    "master_list": "saki_search/saki_master_list.csv",
+    "with_emails": "saki_search/saki_stores_with_emails.csv",
+    "without_emails": "saki_search/saki_stores_without_emails.csv"
 } 
+
+# FILES = {
+#     "search_log": "search_log.csv",
+#     "usage_counter": "usage_counter.txt",
+#     "master_list": "master_list.csv",
+#     "with_emails": "stores_with_emails.csv",
+#     "without_emails": "stores_without_email.csv"
+# } 
