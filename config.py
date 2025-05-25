add3e85a22dc3f39613dcbf2a58ffe1560fc07e4
Devin Reid <dreid97@gmail.com>
2025-05-25 15:09:39 -0500

    Cleaned up program

---
+++ config.py
@@ -0,0 +1,8 @@
+# File Configuration
+FILES = {
+    "search_log": "search_log.csv",
+    "usage_counter": "usage_counter.txt",
+    "master_list": "master_list.csv",
+    "with_emails": "stores_with_emails.csv",
+    "without_emails": "stores_without_email.csv"
+} 
